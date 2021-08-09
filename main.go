a3f23be4be6ec2c94cb11faf4e0144957637d03c
Felix Weilbach <felix.weilbach@t-online.de>
2021-08-09 22:09:41 +0200

    Update header bar tabs on page change
    Signed-off-by: Felix Weilbach <felix.weilbach@t-online.de>

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Task struct {
-	Title  string
-	Detail string
+	Title     string
+	Detail    string
 	TimeSpent string
-	Icon string
+	Icon      string
 }
 
 var tasks = []Task{
-	{Title: "Fix bug #2312", Detail: "Needs to done a specific way.", TimeSpent: "1 hour", Icon: "bi-clock"},
+	{Title: "Fix bug #2312", Detail: "Needs to be done a specific way.", TimeSpent: "1 hour", Icon: "bi-clock"},
 	{Title: "Implement new feature", Detail: "Feature needs to be crazy fancy", TimeSpent: "30 minutes", Icon: "bi-check"},
 	{Title: "Fix the docs", Detail: "Docs need update.", TimeSpent: "2 hours", Icon: "bi-check"},
 	{Title: "Triage issues on GitHub", Detail: "", TimeSpent: "15 minutes", Icon: "bi-check"},
@@ -23,21 +23,28 @@ var tasks = []Task{
 var templates = template.Must(template.ParseFiles("head.html", "tasks.html", "header.html", "footer.html", "reports.html", "about.html", "new_task.html"))
 
 func tasksHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "tasks.html", tasks)
+	err := templates.ExecuteTemplate(w, "tasks.html", struct {
+		Tasks []Task
+		HeaderTabActiveName string
+	}{tasks, "tasks"})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func reportsHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "reports.html", nil)
+	err := templates.ExecuteTemplate(w, "reports.html", struct {
+		HeaderTabActiveName string
+	}{"reports"})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "about.html", nil)
+	err := templates.ExecuteTemplate(w, "about.html", struct {
+		HeaderTabActiveName string
+	}{"about"})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
